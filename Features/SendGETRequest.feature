35582f2038d8db62de69b05979f022b2f8babeba
NaboChristopher <christopher.nabo07@gmail.com>
2024-02-28 23:53:51 +0800

    Initial commit

---
+++ Features/SendGETRequest.feature
@@ -0,0 +1,9 @@
+﻿Feature: Testing API endpoints
+
+    As a tester
+    I want to verify the status code with two included properties of an API endpoint
+
+  Scenario: Verify status code of API endpoint
+    Given I have the API endpoint "https://api.agify.io/?name=meelad"
+    When I send a GET request
+    Then the response status code should be 200
---
+++ StepDefinitions/SendGETRequest.cs
@@ -0,0 +1,35 @@
+﻿using TechTalk.SpecFlow;
+using RestSharp;
+using NUnit.Framework;
+
+[Binding]
+public class APITestSteps
+{
+    private RestClient client;
+    private RestResponse response;
+    private RestRequest request;
+
+    [Given(@"I have the API endpoint ""(.*)""")]
+    public void GivenIHaveTheAPIEndpoint(string endpoint)
+    {
+        client = new RestClient("https://api.agify.io/?name=meelad");
+        request = new RestRequest(endpoint, Method.Get);
+    }
+
+    [When(@"I send a GET request")]
+    public void WhenISendAGETRequest()
+    {
+        response = client.Execute(request);
+    }
+
+    [Then(@"the response status code should be 200")]
+    public void ThenTheResponseStatusShouldBe()
+    {
+        var responseBody = response.Content;
+        Assert.AreEqual(200, (int)response.StatusCode);
+        Assert.IsTrue(responseBody.Contains("count"));
+        Assert.IsTrue(responseBody.Contains("age"));
+    }
+
+
+}
